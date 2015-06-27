f79a0cd82c24418dbeff17db4f0f78deed962f4e
W.-M. Richter <wolfdex@bitbucket.org>
2015-06-27 09:29:38 +0200

    new useful/xrandr-magie.sh

---
+++ useful/xrandr-magie.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# dual
+case "$1" in
+
+ auto) xrandr --output LVDS1 --auto --primary --output VGA1 --auto 
+
+      ;;
+
+
+ dualr) xrandr -o normal && xrandr --output LVDS1 --auto  --output VGA1 --auto  --pos 1280x0
+    
+       ;;
+
+ duall) xrandr -o normal && xrandr --output LVDS1 --auto  --output VGA1 --auto  --pos -1280x0
+
+       ;;
+
+ dualo) xrandr -o normal && xrandr --output LVDS1 --auto  --output VGA1 --auto  --pos 1x0
+
+      ;;
+
+
+ xiv) xrandr -o inverted
+      # NONE CW CCW HALF
+      xsetwacom set "Serial Wacom Tablet eraser" rotate half
+      ;;
+
+ normal) xrandr -o normal
+        xsetwacom set "Serial Wacom Tablet eraser" rotate none
+      ;;
+
+  x270) xrandr -o right
+        xsetwacom set "Serial Wacom Tablet eraser" rotate cw
+      ;;
+
+   *) echo "default" 
+      xrandr -o right
+      ;;
+   
+esac
+
+
+
+
+
